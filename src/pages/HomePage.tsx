a47a56dab8c2efb977d5742e8c312cb7316cb69b
pawan-kavinda <pawankavinda09@gmail.com>
2025-08-03 23:13:12 +0530

    background change

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,6 @@
 // src/pages/Home.jsx
 import React from "react";
-import BannerBackground from "../Assets/home-banner-background.png";
+import BannerBackground from "../Assets/purple.png";
 import RobotImage from "../Assets/robot.png";
 import Navbar from "../components/Navbar";
 import About from "./AboutPage";
@@ -9,6 +9,7 @@ import Footer from "../components/Footer";
 import { Link as ScrollLink } from "react-scroll";
 import ChatWidget from "../components/ChatWidget";
 import Intro from "./IntroPage";
+import FadeInWhenVisible from "../components/FramerMotion";
 
 const Home = () => {
   return (
@@ -18,26 +19,34 @@ const Home = () => {
       {/* Hero Section */}
       <div className="home-banner-container" id="home">
         <div className="home-bannerImage-container">
-          <img src={BannerBackground} alt="Banner Background" />
+          <img
+            sizes="100vw, 500vw"
+            src={BannerBackground}
+            alt="Banner Background"
+          />
         </div>
-        <div className="home-text-section">
-          <h1 className="primary-heading">
-            Create powerful AI Agents Tailored to Your Business
-          </h1>
-          <p className="primary-text">
-            Build AI agents that connect with your APIs, learn from your data,
-            and grow with your users.
-          </p>
-          <ScrollLink to="about" smooth={true} duration={500} offset={-80}>
-            <button className="secondary-button">Learn More</button>
-          </ScrollLink>
-        </div>
+        <>
+          <FadeInWhenVisible>
+            <div className="home-text-section">
+              <h1 className="primary-heading">
+                Create powerful AI Agents Tailored to Your Business
+              </h1>
+              <p className="primary-text">
+                Build AI agents that connect with your APIs, learn from your
+                data, and grow with your users.
+              </p>
+              <ScrollLink to="about" smooth={true} duration={500} offset={-80}>
+                <button className="secondary-button">Learn More</button>
+              </ScrollLink>
+            </div>
+          </FadeInWhenVisible>
+        </>
         <div className="home-image-section">
           <img src={RobotImage} alt="AI Robot" />
         </div>
       </div>
       <ChatWidget />
-      
+
       <section id="intro">
         <Intro />
       </section>
