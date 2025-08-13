24d5decc53efae90dd1ae647f4b01e0e03fb3ba7
pawan-kavinda <pawankavinda09@gmail.com>
2025-08-14 03:56:26 +0530

    side motion

--- src/components/FramerMotion.tsx
+++ src/components/FramerMotion.tsx
@@ -1,16 +1,18 @@
 import { motion, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
-import { useEffect } from "react";
-
-import { ReactNode } from "react";
+import { useEffect, ReactNode } from "react";
 
 interface FadeInWhenVisibleProps {
   children: ReactNode;
   delay?: number;
-  duration?:number;
+  duration?: number;
 }
 
-const FadeInWhenVisible = ({ children, delay,duration = 1.6 }: FadeInWhenVisibleProps) => {
+export const FadeInWhenVisible = ({
+  children,
+  delay,
+  duration = 1.6,
+}: FadeInWhenVisibleProps) => {
   const controls = useAnimation();
   const [ref, inView] = useInView({ triggerOnce: false, threshold: 0.2 });
 
@@ -27,7 +29,7 @@ const FadeInWhenVisible = ({ children, delay,duration = 1.6 }: FadeInWhenVisible
       ref={ref}
       initial="hidden"
       animate={controls}
-      transition={{ duration: duration, delay }}
+      transition={{ duration, delay }}
       variants={{
         visible: { opacity: 1, y: 0 },
         hidden: { opacity: 0, y: 50 },
@@ -38,4 +40,45 @@ const FadeInWhenVisible = ({ children, delay,duration = 1.6 }: FadeInWhenVisible
   );
 };
 
-export default FadeInWhenVisible;
+interface SlideInFromSideProps {
+  children: ReactNode;
+  from?: "left" | "right"; // direction of animation
+  delay?: number;
+  duration?: number;
+}
+
+export const SlideInFromSide = ({
+  children,
+  from = "left",
+  delay = 0,
+  duration = 1.6,
+}: SlideInFromSideProps) => {
+  const controls = useAnimation();
+  const [ref, inView] = useInView({ triggerOnce: false, threshold: 0.2 });
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    } else {
+      controls.start("hidden");
+    }
+  }, [controls, inView]);
+
+  const variants = {
+    hidden: { opacity: 0, x: from === "left" ? -100 : 100 },
+    visible: { opacity: 1, x: 0 },
+  };
+
+  return (
+    <motion.div
+      ref={ref}
+      initial="hidden"
+      animate={controls}
+      transition={{ duration, delay }}
+      variants={variants}
+      style={{ flex: 1 }} // Add this line to preserve flex behavior
+    >
+      {children}
+    </motion.div>
+  );
+};
