9bd95d033f56baaaced7d13c7bc546b2ca801598
pawan-kavinda <pawankavinda09@gmail.com>
2025-09-15 15:54:21 +0530

    dto

--- src/api/dto/auth.dto.ts
+++ src/api/dto/auth.dto.ts
@@ -1,3 +1,41 @@
-export interface LocalLoginReturn{
-    access_token: string;
+export interface LocalLoginReturn {
+  access_token: string;
 }
+
+export type CreateUserPayload = {
+  name: string;
+  contact: string;
+  email: string;
+  field: string;
+};
+
+export type FieldSelectPayload = {
+  uid: string;
+  field: string;
+};
+
+export type FieldAnswerPayload = {
+  uid: string;
+  field: string;
+  questionIndex: number;
+  answer: string;
+  questionId: string;
+};
+
+export type FileUploadPayload = {
+  uid: string;
+  companyName: string;
+  field: string;
+  file: File;
+};
+
+export type AddMoreDataPayload = {
+  uid: string;
+  field: string;
+  message: string;
+};
+
+export type DualAgentsPayload = {
+  uid: string;
+  field: string;
+};
